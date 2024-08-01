7d6d835e885172227a4146ed64e03718607114b8
Dnreikronos <dnreikronos2@gmail.com>
2024-08-01 20:29:47 -0300

    Created the config.toml

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Dnreikronos/wiki-fans-backend/configs"
+	"github.com/Dnreikronos/wiki-fans-backend/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	err := configs.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Get("/", handlers.List)
+	r.Get("/{id}", handlers.Get)
+
+	http.ListenAndServe(fmt.Sprintf(":%s").configs.GetServerPort(), r)
+}
